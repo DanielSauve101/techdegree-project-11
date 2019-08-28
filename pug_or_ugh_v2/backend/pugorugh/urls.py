68f34ce781edb503b8fad491b518c97492c42406
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-08-28 10:46:42 -0400

    initial setup with upgraded versions of django

--- pug_or_ugh_v2/backend/pugorugh/urls.py
+++ pug_or_ugh_v2/backend/pugorugh/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import include, path
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
@@ -9,12 +9,12 @@ from pugorugh.views import UserRegisterView
 
 # API endpoints
 urlpatterns = format_suffix_patterns([
-    url(r'^api/user/login/$', obtain_auth_token, name='login-user'),
-    url(r'^api/user/$', UserRegisterView.as_view(), name='register-user'),
-    url(r'^favicon\.ico$',
+    path('api/user/login/', obtain_auth_token, name='login-user'),
+    path('api/user/', UserRegisterView.as_view(), name='register-user'),
+    path('favicon\.ico',
         RedirectView.as_view(
             url='/static/icons/favicon.ico',
             permanent=True
         )),
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html'))
 ])
