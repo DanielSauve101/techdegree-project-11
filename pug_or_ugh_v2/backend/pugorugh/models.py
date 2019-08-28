7396e0f7d1defc9f749108d4abf7e40dd4dc17e3
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-08-28 13:27:15 -0400

    added models and serializers

--- pug_or_ugh_v2/backend/pugorugh/models.py
+++ pug_or_ugh_v2/backend/pugorugh/models.py
@@ -1,3 +1,68 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from multiselectfield import MultiSelectField
+
+AGE_OPTION = (
+    ('b', 'baby'),
+    ('y', 'young'),
+    ('a', 'adult'),
+    ('s', 'senior'),
+)
+
+GENDER_OPTION = (
+    ('m', 'male'),
+    ('f', 'female'),
+    ('u', 'unknown'),
+)
+
+SIZE_OPTION = (
+    ('s', 'small'),
+    ('m', 'medium'),
+    ('l', 'large'),
+    ('xl', 'extra large'),
+    ('u', 'unknown'),
+)
+
+STATUS_OPTION = (
+    ('l', 'liked'),
+    ('d', 'disliked'),
+)
+
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    image_filename = models.TextField()
+    breed = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    gender = MultiSelectField(max_length=1, choices=GENDER_OPTION, default='u')
+    size = MultiSelectField(max_length=2, choices=SIZE_OPTION, default='u')
+
+    def __str__(self):
+        return self.name
+
+
+class UserDog(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    status = MultiSelectField(max_length=1, choices=STATUS_OPTION)
+
+    def __str__(self):
+        return self.user
+
+
+class UserPref(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    age = MultiSelectField(choices=AGE_OPTION, max_choices=4)
+    gender = MultiSelectField(choices=GENDER_OPTION, max_choices=3)
+    size = MultiSelectField(choices=SIZE_OPTION, max_choices=5)
+
+    def __str__(self):
+        return self.user
+
+
