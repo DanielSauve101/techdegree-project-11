d6ebc8230d829582051bd761039406ce4a73e32b
Daniel Sauve <sauve.daniel.157@gmail.com>
2019-09-18 12:03:19 -0400

    revised tests.py

--- pug_or_ugh_v2/pugorugh/tests.py
+++ pug_or_ugh_v2/pugorugh/tests.py
@@ -75,6 +75,13 @@ class ViewsTest(TestCase):
         view = RetrieveDogView.as_view()
         resp = view(request, pk='1', decision='unknown')
         self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, {'id': 1, 
+                                    'name': 'test_dog',
+                                    'image_filename': '1.jpg',
+                                    'breed': 'retriever', 
+                                    'age': 32, 
+                                    'gender': ['f'], 
+                                    'size': ['l']})
 
     def test_retrieve_liked_dog_view(self):
         factory = APIRequestFactory()
@@ -84,15 +91,27 @@ class ViewsTest(TestCase):
         view = RetrieveDogView.as_view()
         resp = view(request, pk='2', decision='liked')
         self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, {'id': 2, 
+                                    'name': 'test_dog2', 
+                                    'image_filename': '2.jpg', 
+                                    'breed': 'lab', 
+                                    'age': 25, 
+                                    'gender': ['m'], 
+                                    'size': ['m']})
 
     def test_update_dog_status_view(self):
         factory = APIRequestFactory()
+        before_status = UserDog.objects.get(pk=1).status
         request = factory.put(reverse('dog-status',
                                       kwargs={'pk': 1, 'decision': 'liked'}))
         force_authenticate(request, user=self.test_user)
         view = UpdateDogStatusView.as_view()
         resp = view(request, pk='1', decision='liked')
+
+        updated_status = UserDog.objects.get(pk=1).status
         self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(before_status, updated_status)
+        self.assertEqual(updated_status, ['l'])
 
     def test_get_user_preferences_update(self):
         factory = APIRequestFactory()
@@ -101,13 +120,25 @@ class ViewsTest(TestCase):
         view = RetrieveUpdateUserPrefView.as_view()
         resp = view(request)
         self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, {'id': 1, 
+                                    'age': {'s', 'y', 'a', 'b'}, 
+                                    'gender': {'m', 'f'}, 
+                                    'size': {'s', 'm', 'l'}})
 
     def test_put_user_preferences_update(self):
         factory = APIRequestFactory()
-        request = factory.put(reverse('user-pref'))
+        preference_before = UserPref.objects.get(pk=1)
+        request = factory.put(reverse('user-pref'), {'age':'b', 
+                                                    'gender':'m', 
+                                                    'size':'l'})
         force_authenticate(request, user=self.test_user)
         view = RetrieveUpdateUserPrefView.as_view()
         resp = view(request, age='b', gender='m', size='l')
+
+        preference_after = UserPref.objects.get(pk=1)
         self.assertEqual(resp.status_code, 200)
+        self.assertNotEqual(preference_before.age, preference_after.age)
+        self.assertNotEqual(preference_before.gender, preference_after.gender)
+        self.assertNotEqual(preference_before.size, preference_after.size)
 
         
